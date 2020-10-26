94eb91f708ed82b2906ca94a95e100a68f6908ef
Shkryabminer <ivolodko85@gmail.com>
2020-10-26 12:03:16 +0200

    firstCommit

---
+++ MapNotePad/ViewModels/BaseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MapNotePad.ViewModels
+{
+    public class BaseViewModel : BindableBase, INavigationAware
+    {
+
+       public readonly INavigationService NavigationService;
+
+        public BaseViewModel(INavigationService navigationService)
+        {
+            NavigationService = navigationService;
+        }
+        
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
